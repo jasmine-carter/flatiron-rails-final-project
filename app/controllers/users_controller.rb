1fd5585183636b122072c3a223984f498cfa1557
Jasmine Carter <heyjasminec@gmail.com>
2020-11-08 16:32:48 -0500

    Create show user view, create logic in show user page to only allow for logged in users to access, otherwise redirect to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.create(user_params)
     return redirect_to controller: 'users', action: 'new' unless @user.save
     session[:user_id] = @user.id
@@ -28,6 +27,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
   end
 
 end
