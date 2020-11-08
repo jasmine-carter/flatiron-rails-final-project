d9a89e41360f51d5e330b2cfbab5e7d647f8c183
Jasmine Carter <heyjasminec@gmail.com>
2020-11-08 16:00:50 -0500

    Create new user action using user params, create private method user params, creaet current_user method in application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    binding.pry
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    @user.save
+    redirect_to @user
   end
 
 
@@ -12,7 +18,13 @@ class UsersController < ApplicationController
   end
 
   def show
+    #build out current user method to check if user is logged in, if not, redirect to new user view
+    @user = User.find_by(id: params[:id])
   end
 
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 
 end
