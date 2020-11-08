9ed16608c5f4962a849002b24963b27d505a4d12
Jasmine Carter <heyjasminec@gmail.com>
2020-11-08 16:46:59 -0500

    Add private method user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,14 +24,15 @@ class UsersController < ApplicationController
     end
   end
 
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
-  end
-
   def destroy
     session.clear
     redirect_to root_path
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 
 end
