fc5c760bb9daa72d98b018af04c8032e7fa503c1
Jasmine Carter <heyjasminec@gmail.com>
2020-11-09 13:01:22 -0500

    Create WorkoutController, fill out new workout action

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,20 @@
+class WorkoutsController < ApplicationController
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+
+end
