ec26d73b8a2c4ac70db08a3b1b22673b60174df3
Jasmine Carter <heyjasminec@gmail.com>
2020-11-08 20:41:48 -0500

    Extend current_user method in ApplicationController to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-
+  helper_method :current_user
+  
   def current_user
     if session[:user_id].present?
       @user = User.find_by(id: session[:user_id])
     end
   end
 
-  
+
 end
