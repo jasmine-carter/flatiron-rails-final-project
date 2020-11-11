c9432074852f6a15f5efaa61d5cc79d42ea997a5
Jasmine Carter <heyjasminec@gmail.com>
2020-11-10 19:24:51 -0500

    Correct workoutexercise attribute assignment from params, add workout.user_id assignment

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -14,19 +14,22 @@ class WorkoutsController < ApplicationController
   def create
     #create logic that assignes @workout.user_id == current_user.id
     #create logic that only users that are logged in can create workouts
-    binding.pry
     @workout = Workout.create(workout_params)
     params[:workout][:workout_exercises_attributes].values.each do |we|
-      we = WorkoutExercise.create(exercise_id: workoutex["exercise_id"], workout_id: @workout.id, reps: workoutex["reps"], sets: workoutex["sets"])
-    we.save
-    #note right now exercises do have many workouts (ie exercise.workouts works), but workout.exercises does not work
+      we = WorkoutExercise.create(exercise_id: we["exercise_id"], workout_id: @workout.id, reps: we["reps"], sets: we["sets"])
+      we.save
     end
+    @workout.user_id = current_user
+    @workout.save
+    redirect_to @workout
   end
 
   def show
+
   end
 
   def edit
+    #only allow owner of workout to edit
   end
 
   def delete
