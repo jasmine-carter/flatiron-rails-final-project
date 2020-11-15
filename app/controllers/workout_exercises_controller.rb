d8af050eda3414f343ac0929ea71e4f8ba5526e7
Jasmine Carter <heyjasminec@gmail.com>
2020-11-15 13:56:57 -0500

    Add ability to create a new exercise workout through an existing exercise

--- app/controllers/workout_exercises_controller.rb
+++ app/controllers/workout_exercises_controller.rb
@@ -1,6 +1,8 @@
 class WorkoutExercisesController < ApplicationController
 
   def new
+    @workout_exercise = WorkoutExercise.new
+    @workout = Workout.find_by(id: params[:workout_id])
   end
 
   def create
