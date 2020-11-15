33ca95cb03f4bbe4d542112a3bda1c8e4ae53dc2
Jasmine Carter <heyjasminec@gmail.com>
2020-11-15 14:34:46 -0500

    Enable nested resource workout exercise to be created under its workout

--- app/controllers/workout_exercises_controller.rb
+++ app/controllers/workout_exercises_controller.rb
@@ -1,14 +1,22 @@
 class WorkoutExercisesController < ApplicationController
 
   def new
-    @workout_exercise = WorkoutExercise.new
+    @workout_exercise = WorkoutExercise.new(workout_id: params[:workout_id])
     @workout = Workout.find_by(id: params[:workout_id])
   end
 
   def create
+    #add validations
+    @workout_exercise = WorkoutExercise.create(workout_exercise_params)
+    @workout_exercise.save
   end
 
   def delete
   end
+
+  def workout_exercise_params
+    params.require(:workout_exercise).permit(:reps, :sets, :exercise_id, :workout_id)
+  end
+
 
 end
