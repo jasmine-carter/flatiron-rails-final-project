138836cee804342e9f85c1770c7f1e866b18b0d1
Jasmine Carter <heyjasminec@gmail.com>
2020-11-15 20:46:58 -0500

    Create login method and require login method to be used to force login for certain controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-  
+
   def current_user
     if session[:user_id].present?
       @user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in?
+    current_user
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_user
     end
   end
 
