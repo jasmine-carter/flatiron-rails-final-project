5ce6bb50905c550afb76e708752df612fab01139
Jasmine Carter <heyjasminec@gmail.com>
2020-11-17 22:53:59 -0500

    refactor create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    return redirect_to controller: 'users', action: 'new' unless @user.save
-    session[:user_id] = @user.id
-    @user.save
-    redirect_to @user
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
 
