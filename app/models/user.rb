6ccaccf84ce36ccd01368157ea8bc4089114ee78
Jasmine Carter <heyjasminec@gmail.com>
2020-11-17 22:54:28 -0500

    use new error partial for user sign in errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   has_many :exercises
 
   validates :email, uniqueness: true, presence: true
+  validates :name, presence: true
+  validates :password, presence: true
 end
