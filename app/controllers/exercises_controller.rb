44c44d1d43d66de503bfa12662d4ef28a593a861
Jasmine Carter <heyjasminec@gmail.com>
2020-11-17 21:23:38 -0500

    Allow for users to view exercises by musclegroup id, nested resource show requirement

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,12 @@ class ExercisesController < ApplicationController
   before_action :require_login
 
   def index
-    @exercises = Exercise.all
+    if params.has_key?("muscle_group_id")
+      @muscle_group = MuscleGroup.find_by(id: params[:muscle_group_id])
+      @exercises = @muscle_group.exercises
+    else
+      @exercises = Exercise.all
+    end
   end
 
   def new
