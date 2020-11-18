000648342ca9cd4c999bb2ae18ebb17bbbcb9da1
Jasmine Carter <heyjasminec@gmail.com>
2020-11-17 21:58:21 -0500

    Refactor current user method to use ||= to only call db once

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    if session[:user_id].present?
-      @user = User.find_by(id: session[:user_id])
-    end
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 
   def logged_in?
