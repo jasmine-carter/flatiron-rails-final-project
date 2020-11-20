fdce0d5f26df2d6cbfd547c93630de5b0312ff0e
Jasmine Carter <heyjasminec@gmail.com>
2020-11-20 15:25:14 -0500

    correct implementation of custom route for swoliest user

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'static#home'
-  get '/users/swoliest' => 'users#swoliest'
   get '/auth/facebook/callback' => 'session#create' #correct session naming convention to pluralize
   get '/signin', to: 'session#new'
   post '/signin', to: 'session#create'
   delete '/logout', to: 'session#destroy'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  resources :users, only: [:swoliest, :show, :index]
+
+  resources :users do
+    get 'swoliest', on: :collection
+  end
+
   resources :exercises
   resources :workouts do
     resources :workout_exercises, only: [:show, :index, :edit, :new, :delete]
