1090db95c2fca0bd77566cc8fb0af3fb641bcc18
Jasmine Carter <heyjasminec@gmail.com>
2020-11-19 19:14:38 -0500

    Adjust user routes and order of routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   root 'static#home'
   get '/users/swoliest' => 'users#swoliest'
-  resources :users, only: [:swoliest, :show]
+  get '/auth/facebook/callback' => 'session#create' #correct session naming convention to pluralize
+  get '/signin', to: 'session#new'
+  post '/signin', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users, only: [:swoliest, :show, :index]
   resources :exercises
   resources :workouts do
     resources :workout_exercises, only: [:show, :index, :edit, :new, :delete]
@@ -13,10 +19,5 @@ Rails.application.routes.draw do
     resources :exercises, only: [:show, :index, :new]
   end
 
-  get '/auth/facebook/callback' => 'session#create' #correct session naming convention to pluralize
-
   resources :workout_exercises
-  get '/signin', to: 'session#new'
-  delete '/logout', to: 'session#destroy'
-  post '/signin', to: 'session#create'
 end
